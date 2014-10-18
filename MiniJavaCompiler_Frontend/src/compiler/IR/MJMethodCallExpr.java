bcc35a60932d3ec2d48ea6c793690e259d975d85
Malte <Malte@Malte>
2014-10-18 20:52:17 +0100

    methodcallexpr

--- MiniJavaCompiler_Frontend/src/compiler/IR/MJMethodCallExpr.java
+++ MiniJavaCompiler_Frontend/src/compiler/IR/MJMethodCallExpr.java
@@ -5,12 +5,25 @@ import java.util.LinkedList;
 import compiler.PrettyPrinter;
 
 public class MJMethodCallExpr extends MJExpression {
-	
+	private String name;
+	private String methodName;
+	private LinkedList<MJExpression> arguments;
+	private int i = 1;
 	
 	public MJMethodCallExpr(String name, String methodName, LinkedList<MJExpression> arguments) {
+		this.name = name;
+		this.methodName = methodName;
+		this.arguments = arguments;
 	}
 
 	public void prettyPrint(PrettyPrinter prepri) {
-		// TODO prettyPrint MethodCallExpr
+		prepri.print(name + "." + methodName + "(");
+		this.arguments.get(0).prettyPrint(prepri);
+		while (i <= arguments.size()){
+			prepri.print(", ");
+			this.arguments.get(i).prettyPrint(prepri);
+			i++;
+		}
+		prepri.print(")");
 	}
 }
