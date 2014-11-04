e18d5236d7bf2a3d0b7e8144b65baf28f9c44553
bakhoj <anders2108@gmail.com>
2014-11-04 14:36:03 +0100

    succestest1

---
+++ MiniJavaCompiler_Frontend/tests/succesTest1.java
@@ -0,0 +1,23 @@
+
+public class succesTest1 {
+
+	public static void main(String[] args) {
+		int int_a = 1;
+		int int_b = 4;
+		int int_c = 1;
+		String string_a = "Done";
+
+		if(string_a ="Done" && this.int_a == this.int_c){
+			
+			this.int_a = int_a + 1
+					
+			while(this.int_a < this.int_b){
+				int_a = int_a * 2
+				
+			}
+			System.out.println(string_a + "\n" + "int_a: " + int_a + "\nint_b: " + int_b + "\nint_c: " + int_c);
+
+		}
+	}
+
+}
